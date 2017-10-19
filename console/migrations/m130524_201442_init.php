fcd14102ab89d19ceb075e726379edf580c9ad43
scepsis <f.senin@nextcontact.su>
2017-10-19 17:47:04 +0300

    Настройки композера и наработки миграции

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -12,6 +12,7 @@ class m130524_201442_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
+        //Пользователи
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
@@ -24,10 +25,41 @@ class m130524_201442_init extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        //Категории к которым будут относиться статьи
+        $this->createTable('ds_categories', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull()->unique()->comment('Техническое название категории'),
+            'label' => $this->string()->notNull()->unique()->comment('Название категории'),
+            'description' => $this->string()->null()->comment('Краткое описание категории'),
+            'parent_id' => $this->integer()->null()->comment('id родительской категории'),
+            'is_active' => $this->boolean()->defaultValue(0)->comment('Активна ли категория')
+        ]);
+
+        $this->addCommentOnTable('ds_categories', 'Таблица с категориями, к которым могут принадлежать статьи');
+        $this->addForeignKey('fk_fid_parent_category', 'ds_categories', 'parent_id', 'ds_categories', 'id');
+
+        //Статьи
+        $this->createTable('ds_posts', [
+            'id' => $this->primaryKey(),
+            'fid_category' => $this->integer()->notNull()->comment('id категории к которой принадлежит статья'),
+            'title' => $this->string()->notNull()->comment('Техническое название статьи'),
+            'label' => $this->string()->notNull()->comment('Название статьи'),
+            'content' => $this->text()->null()->comment('Контент статьи в виде html-разметки'),
+            'views_count' => $this->integer()->notNull()->defaultValue(0)->comment('Количество просмотров'),
+            'fid_user' => $this->integer()->notNull()->comment('id пользователя создавшего статью'),
+            'is_active' => $this->boolean()->notNull()->defaultValue(0)->comment('Активна ли статья'),
+        ]);
+
+        $this->addCommentOnTable('ds_posts', 'Таблица со статьями');
+        $this->addForeignKey('fk_posts_fid_category', 'ds_posts', 'fid_category', 'ds_categories', 'id');
     }
 
     public function down()
     {
+        $this->dropTable('ds_posts');
+        $this->dropTable('ds_categories');
         $this->dropTable('{{%user}}');
+
     }
 }
